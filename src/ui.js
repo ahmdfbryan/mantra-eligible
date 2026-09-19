679d1cd3ebb955332b70b312600ab1b64c4e5b88
ahmdfbryan <okangrian@gmail.com>
2026-09-19 16:00:03 +0700

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -29,7 +29,7 @@ function buildProgressBar(current, total, length = 14) {
 
 function authorConfig(member) {
   return {
-    name: `${config.community.name} - Verification`,
+    name: `${config.community.name}`,
     iconURL: member?.groupIconUrl || undefined,
     url: config.community.url,
   };
