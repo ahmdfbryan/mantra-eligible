a35ed3e5be09a650d65ab9d42f5a2354d1f7ac26
ahmdfbryan <okangrian@gmail.com>
2026-09-26 03:08:50 +0700

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -19,6 +19,48 @@ const commands = [
     .setName('unpanel')
     .setDescription('Lepas panel "Cek Status Akun" dari channel ini')
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+  new SlashCommandBuilder()
+    .setName('quiz-menang')
+    .setDescription('Tambah poin Quiz Arena untuk pemenang (leaderboard & role top 3 auto update)')
+    .addUserOption((option) =>
+      option.setName('user').setDescription('User yang menang quiz').setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName('poin')
+        .setDescription('Jumlah poin yang ditambahkan (default 1)')
+        .setMinValue(1)
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+  new SlashCommandBuilder()
+    .setName('quiz-kurang')
+    .setDescription('Kurangi poin Quiz Arena user (buat koreksi)')
+    .addUserOption((option) =>
+      option.setName('user').setDescription('User yang mau dikurangi poinnya').setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName('poin')
+        .setDescription('Jumlah poin yang dikurangi (default 1)')
+        .setMinValue(1)
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+  new SlashCommandBuilder()
+    .setName('quiz-set')
+    .setDescription('Set poin Quiz Arena user langsung ke nilai tertentu (koreksi/import data)')
+    .addUserOption((option) =>
+      option.setName('user').setDescription('User yang mau di-set poinnya').setRequired(true)
+    )
+    .addIntegerOption((option) =>
+      option.setName('poin').setDescription('Poin baru').setMinValue(0).setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+  new SlashCommandBuilder()
+    .setName('quiz-leaderboard')
+    .setDescription('Pasang/pindahkan panel Quiz Arena Leaderboard (live, auto-update) ke channel ini')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(config.discordToken);
